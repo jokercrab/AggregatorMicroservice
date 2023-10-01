d6596762ce9c3827f55d9985b59c86a72b16de07
Tomishiko <anton20435@outlook.com>
2023-10-01 18:52:32 +0300

    configured urls and cors

--- Program.cs
+++ Program.cs
@@ -1,9 +1,25 @@
 using Aggregator.AuxiliaryServices;
 using Aggregator.DataFetchers;
+using MongoDB.Driver;
+using MongoDB.Bson;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 
-var builder = WebApplication.CreateBuilder(args);
 
+
+
+var builder = WebApplication.CreateBuilder(args);
+//CORS
+var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy  =>
+                      {
+                          policy.WithOrigins("http://10.42.0.207:3000",
+                                                "http://10.42.0.1",
+                                                "http://localhost:3000");
+                      });
+});
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -27,6 +43,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.UseCors(MyAllowSpecificOrigins);
 
 app.MapControllers();
 
