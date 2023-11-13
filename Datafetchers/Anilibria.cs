977dc0b7cddf57a45ac2d33a5353c79479e67df4
Tomishiko <anton20435@outlook.com>
2023-11-13 19:17:01 +0200

    iasyncenumerable implementation

--- Datafetchers/Anilibria.cs
+++ Datafetchers/Anilibria.cs
@@ -4,12 +4,11 @@ using Aggregator.DataStructs;
 using Microsoft.AspNetCore.WebUtilities;
 using System.Collections;
 using System.Net.Http.Headers;
-
 namespace Aggregator.DataFetchers;
 
-public class Anilibria
+public class Anilibria:IDataFetcher
 {
-    private readonly HttpClient _httpClient;
+    private readonly HttpClient _httpClient; 
     private const string _baseURI = "https://api.anilibria.tv/";
     public Anilibria(HttpClient httpClient)
     {
@@ -18,7 +17,7 @@ public class Anilibria
         _httpClient.DefaultRequestHeaders.Add("User-Agent", "Anything");
         _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
     }
-    public async Task<IEnumerable<Anime>?> GetUpdates(int limit)
+    public async IAsyncEnumerable<Anime> GetUpdates()
     {
         JsonNode? json;
         var timestamp = DateTimeOffset.Now.AddDays(-7).ToUnixTimeSeconds();
@@ -27,7 +26,7 @@ public class Anilibria
             new Dictionary<string, string?>{
                 {"filter", "code,names,posters,status,type,genres,player.episodes,updated"},
                 {"since", timestamp.ToString()},
-                {"limit",limit.ToString()}
+                {"limit","30"}
             });
         try
         {
@@ -43,18 +42,19 @@ public class Anilibria
         }
 
         if (json == null)
-            return null;
+            yield break;
         var jsonResult = json.Deserialize<AnilibraiDataStruct[]>
         (new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
 
         if(jsonResult == null)
-            return null;
+            yield break;
         var result = new Anime[jsonResult.Length];
         for(int i=0;i<result.Length;i++)
         {
-            result[i] = new Anime(jsonResult[i]);
+            //result[i] = new Anime(jsonResult[i]);
+            yield return new Anime(jsonResult[i]);
         }
-        return result;
+        
     }
     private DateTime SecondsToDateTime(long timestamp, bool local = false)
     {
